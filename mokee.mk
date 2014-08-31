a544cf2ffa6df7d24932d54b9b6695bf61e55a36
robkingalex <robkingalex@yahoo.com>
2014-08-31 16:40:39 +0700

    update mk

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
 TARGET_KERNEL_CONFIG := gee_defconfig
 
@@ -7,11 +7,11 @@ TARGET_KERNEL_CONFIG := gee_defconfig
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/gee/cm_gee.mk)
+$(call inherit-product, device/lge/gee/mk_gee.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gee
@@ -20,7 +20,7 @@ PRODUCT_BRAND := LGE
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.2 KVT49L 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.4.2 KVT49L 937116 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
